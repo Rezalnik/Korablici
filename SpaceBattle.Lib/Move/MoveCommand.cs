609f3e52a6f4dcbf83fcc13325b2714e8538c792
Vekselman Roman <vekselmanrv@mail.ru>
2022-10-24 11:46:34 +0000

    "Создание интерфейсов IMove, ICommand, Создание Классов MoveCommand, Vectore"

---
+++ SpaceBattle.Lib/Move/MoveCommand.cs
@@ -0,0 +1,15 @@
+//using SpaceBattle.Lib;
+namespace SpaceBattle.Lib;
+
+public class MoveCommand : ICommand
+{
+    private IMove obj;
+    public MoveCommand(IMove obj){
+        this.obj = obj;
+    }
+    public void Execute()
+    {
+        obj.Position = obj.Position + obj.Velocity;
+    }
+    //public void Execute() => this.obj.Position += this.obj.Velocity;
+}
---
+++ SpaceBattle.Lib/Vector.cs
@@ -0,0 +1,117 @@
+﻿namespace SpaceBattle.Lib;
+
+
+public class Vector
+{
+    public int[] v;
+
+    public Vector(params int[] args)
+    {
+        v = args;
+    }
+    public int Size()
+    {
+        return v.Length;
+    }
+    public override string ToString()
+    {
+        var output = "Vector(";
+        for (var i = 0; i < v.Length - 1; i++)
+        {
+            output += v[i].ToString() + ", ";
+        }
+        output += v[v.Length - 1].ToString() + ")";
+        return output;
+    }
+    public override bool Equals(object? obj)
+    {
+        return obj is Vector vector &&
+               EqualityComparer<int[]>.Default.Equals(v, vector.v);
+    }
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(v);
+    }
+    public int this[int index]
+    {
+        get
+        {
+            return v[index];
+        }
+    }
+    public static Vector operator +(Vector v1, Vector v2)
+    {
+        if (v1.Size() == v2.Size())
+        {
+            int[] v3 = new int[v1.Size()];
+            for (var i = 0; i < v1.Size(); i++)
+            {
+                v3[i] = v1[i] + v2[i];
+            }
+            return new Vector(v3);
+        }
+        else throw new ArgumentException("Вектора имеют разные длины!");
+    }
+    public static Vector operator -(Vector v1, Vector v2)
+    {
+        if (v1.Size() == v2.Size())
+        {
+            int[] v3 = new int[v1.Size()];
+            for (var i = 0; i < v1.Size(); i++)
+            {
+                v3[i] = v1[i] - v2[i];
+            }
+            return new Vector(v3);
+        }
+        else throw new ArgumentException("Вектора имеют разные длины!");
+    }
+    public static Vector operator *(int s, Vector v1)
+    {
+        int[] v2 = new int[v1.Size()];
+        for (var i = 0; i < v1.Size(); i++)
+        {
+            v2[i] = s * v1[i];
+        }
+        return new Vector(v2);
+    }
+    public static Vector operator *(Vector v, int s)
+    {
+        return s * v;
+    }
+    public static bool operator ==(Vector v1, Vector v2)
+    {
+        var isequal = true;
+        if (v1.Size() == v2.Size())
+        {
+            for (var i = 0; i < v1.Size(); i++)
+            {
+                isequal = isequal && (v1[i] == v2[i]);
+            }
+        }
+        else isequal = false;
+        return isequal;
+    }
+    public static bool operator !=(Vector v1, Vector v2)
+    {
+        return !(v1 == v2);
+    }
+    public static bool operator <(Vector v1, Vector v2)
+    {
+        var isless = true;
+        var l = true;
+        for (int i = 0; i < Math.Min(v1.Size(), v2.Size()); i++)
+        {
+            isless = isless && !(v1[i] > v2[i]);
+            if (v1[i] < v2[i]) l = false;
+        }
+        if (l && isless)
+        {
+            if (v1.Size() >= v2.Size()) isless = false;
+        }
+        return isless;
+    }
+    public static bool operator >(Vector v1, Vector v2)
+    {
+        return v2 < v1;
+    }
+}
