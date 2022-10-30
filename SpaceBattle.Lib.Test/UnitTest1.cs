1ab5f82f7ed27d979ddeb2c340d474e7c9d525c2
Vekselman Roman <vekselmanrv@mail.ru>
2022-10-30 11:00:27 +0000

    "Тесты - работающая реализация с костылями"

--- SpaceBattle.Lib.Test/UnitTest1.cs
+++ SpaceBattle.Lib.Test/UnitTest1.cs
@@ -6,29 +6,50 @@ namespace SpaceBattle.Lib.Test;
 public class MoveCommandTests
 {
     [Fact]
-    public void TestPositiveMove()
+    public void TestPositive()
     {
 
-        var m = new Mock<IMove>();
-        m.Setup(a => a.Position).Returns(new Vector(12, 5)).Verifiable();
-        m.Setup(a => a.Velocity).Returns(new Vector(-7, 3)).Verifiable();
-        var c = new MoveCommand(m.Object);
+        // var m = new Mock<IMove>();
+        // m.Setup(a => a.Position).Returns(new Vector(12, 5)).Verifiable();
+        // m.Setup(a => a.Velocity).Returns(new Vector(-7, 3)).Verifiable();
+        // var c = new MoveCommand(m.Object);
         
-        c.Execute();
+        // c.Execute();
 
-        m.VerifySet(a => a.Position = new Vector(5, 8), Times.Once);
-        m.VerifyAll();
+        // m.VerifySet(a => a.Position = new Vector(5, 8), Times.Once);
+        // m.VerifyAll();
 
-        // //PRE
-        // Mock<IMove> movable = new Mock<IMove>();
-        // movable.SetupProperty(m => m.Position, new Vector(0, 0));
-        // movable.SetupGet<Vector>(m => m.Velocity).Returns(new Vector(1, 1));
-        // MoveCommand command = new MoveCommand(movable.obj);
+        //PRE
+        Mock<IMove> movable = new Mock<IMove>();
+        movable.SetupProperty(m => m.Position, new Vector(12, 5));
+        movable.SetupGet(m => m.Velocity).Returns(new Vector(-7, 3));
+        
 
-        // //ACTION
-        // command.Execute();
+        //ACTION
+        MoveCommand command = new MoveCommand(movable.Object);
+        command.Execute();
 
-        // //POST
-        // Assert.Equal(new Vector(1, 1), movable.obj.Position);
+        //POST
+        var q = new Vector(5, 8); // костыли. Причина использования - кривой класс Vector
+        var w = (q[0], q[1]);
+        var e = (movable.Object.Position[0], movable.Object.Position[1]);
+        Assert.Equal(w, e);
     }
+
+    // [Theory]
+    // public void TestNegSetPosition()
+    // {
+    //     //PRE
+    //     Mock<IMove> movable = new Mock<IMove>();
+    //     movable.SetupProperty(m => m.Position, new Vector(12, 5));
+    //     movable.SetupGet(m => m.Velocity).Returns(new Vector(-7, 3));
+    //     movable.SetupSet(m => m.Position = It.IsAny<Vector>()).Throws<Exception>();
+    //     MoveCommand command = new MoveCommand(movable.Object);
+
+    //     //ACTION
+    //     command.Execute();
+
+    //     //POST
+    //     Assert.Throws<Exception>(() =>  command.Execute());
+    // }
 }
