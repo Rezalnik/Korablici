904696515c3d7c8e63ed193588d43db71be26be1
Vekselman Roman <vekselmanrv@mail.ru>
2023-01-10 05:59:28 +0000

    Create, Change: Tree.cs, TreeNode.cs and Tests

---
+++ SpaceBattle.Lib/CSVReader.cs
@@ -0,0 +1,32 @@
+namespace SpaceBattle.Lib;
+
+public class CSVReader: ICommand
+{
+    private string spliter;
+    private string fileway;
+    private List<Dictionary<string, object?>> table;
+    public CSVReader(string fileway, string spliter="; ")
+    {
+        this.table = new List<Dictionary<string, object?>>();
+        this.fileway = fileway;
+        this.spliter = spliter;
+    }
+
+    public void Execute()
+    {
+        using (var reader = new StreamReader(fileway))
+        {
+            IList<string> listHEADERS = reader.ReadLine().Split("; ");
+            
+            while (!reader.EndOfStream)
+            {
+                string[] values = reader.ReadLine().Split("; ");
+                table.Add(new Dictionary<string, object?>(listHEADERS.Zip(values, (k, v) => new KeyValuePair<string, object?>(k, (object?)v))));
+            }
+        }
+    }
+
+    public List<Dictionary<string, object?>> get_table(){
+        return this.table;
+    }
+}
