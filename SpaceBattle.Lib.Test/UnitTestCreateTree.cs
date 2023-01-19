e4be3b0c6ab60375494ad24746fcc5c2ccb1f811
Vekselman Roman <vekselmanrv@mail.ru>
2023-01-19 23:55:18 +0000

    Change Tests, CreateTreefromtxt files

--- SpaceBattle.Lib.Test/UnitTestCreateTree.cs
+++ SpaceBattle.Lib.Test/UnitTestCreateTree.cs
@@ -7,30 +7,50 @@ namespace SpaceBattle.Lib.Test;
 
 public class TestCreateTree
 {
-    public void TestTree()
+    Mock<IStrategy> CreateTreefromtxtStrategy = new Mock<IStrategy>();
+    public TestCreateTree()
     {
         new InitScopeBasedIoCImplementationCommand().Execute();
-
-        var tree = new Mock<ICommand>();
-        tree.Setup(t => t.Execute());
-
-        var returnTree = new Mock<IStrategy>();
-        returnTree.Setup(t => t.ExecuteStrategy(It.IsAny<string>())).Returns(tree.Object);
-
-        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "GetCollisionTree", (object[] args) => returnTree.Object.ExecuteStrategy(args)).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "GetCollisionTree", (object[] args) => CreateTreefromtxtStrategy.Object.ExecuteStrategy(args)).Execute();
     }
 
     [Fact]
-    public void TestPositive()
-    {
-        //PRE
+    public void TestPositive(){
+
+        string way = @"../../../../SpaceBattle.Lib.Test/CollisionVectors.txt";
+        CreateTreefromtxtStrategy.Setup(t => t.ExecuteStrategy(It.IsAny<object[]>())).Returns(new Dictionary<string, object>()).Verifiable();
+
+        var Tree = new CreateTreeFromtxt_2();
+        Tree.ExecuteStrategy(way);
         
+        CreateTreefromtxtStrategy.Verify();
+    }
 
+    [Fact]
+    public void TestNegFileNotFound(){
+        var way = @"../../../../not_existed.txt";
+        CreateTreefromtxtStrategy.Setup(t => t.ExecuteStrategy(It.IsAny<object[]>())).Returns(new Dictionary<string, object>()).Verifiable();
+        var Tree = new CreateTreeFromtxt_2();
+        Assert.Throws<Exception>(() => Tree.ExecuteStrategy(way));
+        CreateTreefromtxtStrategy.Verify();
+    }
 
-        //ACTION
+    [Fact]
+    public void TestNegArgument(){
+        string way = @"";
+        CreateTreefromtxtStrategy.Setup(t => t.ExecuteStrategy(It.IsAny<object[]>())).Returns(new Dictionary<string, object>()).Verifiable();
+        var Tree = new CreateTreeFromtxt_2();
+        Assert.Throws<Exception>(() => Tree.ExecuteStrategy(way));
+        CreateTreefromtxtStrategy.Verify();
+    }
 
-
-        //POST
-
+    [Fact]
+    public void TestNegDirectoryNotFound(){
+        string way = @"../../../../folder_not_exist/CollisionVectors.txt";
+        CreateTreefromtxtStrategy.Setup(t => t.ExecuteStrategy(It.IsAny<object[]>())).Returns(new Dictionary<string, object>()).Verifiable();
+        var Tree = new CreateTreeFromtxt_2();
+        Assert.Throws<Exception>(() => Tree.ExecuteStrategy(way));
+        CreateTreefromtxtStrategy.Verify();
     }
 }
--- SpaceBattle.Lib/CreateTreefromtxt_2.cs
+++ SpaceBattle.Lib/CreateTreefromtxt_2.cs
@@ -6,23 +6,32 @@ public class CreateTreeFromtxt_2 : IStrategy
 {
     public object ExecuteStrategy(params object[] args)
     {
-        using (var fileReader = new StreamReader((string)args[0]))
+
+        var CollisionTree = IoC.Resolve<Dictionary<string, object>>("GetCollisionTree");
+        StreamReader fileReader;
+
+        try
         {
-            var CollisionTree = IoC.Resolve<Dictionary<string, object>>("GetCollisionTree");
-
-            while (!fileReader.EndOfStream)
-            {
-                string[] values = fileReader.ReadLine()!.Split(" ");
-
-                var branch = CollisionTree;
-
-                foreach (string value in values)
-                {
-                    branch.TryAdd(value, new Dictionary<string, object>());
-                    branch = (Dictionary<string, object>) branch[value];
-                }
-            }
-            return CollisionTree;
+            fileReader = new StreamReader((string)args[0]);
         }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.ToString());
+        }
+
+        while (!fileReader.EndOfStream)
+        {
+            string[] values = fileReader.ReadLine()!.Split(" ");
+
+            var branch = CollisionTree;
+
+            foreach (string value in values)
+            {
+                branch.TryAdd(value, new Dictionary<string, object>());
+                branch = (Dictionary<string, object>) branch[value];
+            }
+        }
+        return CollisionTree;
+        
     }
 }
